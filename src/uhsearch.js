86e50dfc7c55d2887cd9681c5a32d4d0beb3ca30
Jussi Rytkönen <jussi.rytkonen@gmail.com>
2016-01-08 12:04:19 +0200

    UI: for -loops and rerendering stuff

--- src/uhsearch.js
+++ src/uhsearch.js
@@ -1,4 +1,4 @@
-import {ComponentMetadata as Component, ViewMetadata as View} from 'angular2/angular2';
+import {ComponentMetadata as Component, ViewMetadata as View, NgZone} from 'angular2/angular2';
 
 @Component({
   selector: 'uhsearch'
@@ -12,9 +12,14 @@ export class uhsearch {
 
   constructor() {
     console.info('uhsearch Component Mounted Successfully');
+    this.searchList = [
+      { title: 'eka',  description: 'ekan selska' },
+      { title: 'toka', description: 'tokan selska' },
+      { title: 'koka', description: 'kokan selska' }
+    ];
   }
 
   search(searchInput) {
-    console.log(searchInput.value);
+    this.searchList.push({ title: searchInput.value })
   }
 }
