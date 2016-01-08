b211c81dbb8cb3b46e8f5ac04bd07a7e7cff0eb0
Jani Palsamäki <jani.palsamaki@gmail.com>
2016-01-08 10:59:23 +0200

    Initial search support.

--- server/service/search.service.js
+++ server/service/search.service.js
@@ -1,8 +1,15 @@
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('./server.properties');
 
+var request = require('request');
+
 function search(term) {
-  properties.get();
+  var searchUrl = properties.get('search.url');
+  searchUrl += '/focus/api?method=searchc&lang=fi&format=json&max=10&query=';
+
+  request(searchUrl + term, function (error, response, body) {
+    console.log(body);
+  });
 }
 
 module.exports = {
